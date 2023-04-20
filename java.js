f2eef06445ed328b3b861177865ff2d480186bfc
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-04-20 19:45:29 -0300

    make computer choice

---
+++ java.js
@@ -0,0 +1,28 @@
+
+    function getComputerChoice(){
+    
+    let random = Math.floor(Math.random() * 3);
+
+    if(random === 0){
+
+         = "Rock";
+
+    }else if(random === 1){
+
+         = "Paper";
+
+    }else{
+
+         = "Scissors";
+
+    }
+
+}
+
+    const computerSelection= getComputerChoice;
+
+    console.log(computerSelection);
+
+
+
+
