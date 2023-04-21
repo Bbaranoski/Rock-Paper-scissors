6e2601a3d65cf9d789fcdc9db126dac58c3832ec
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-04-20 22:44:41 -0300

    finishing the game

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+    for(i=1; i<=5; i++){
 
     function getComputerChoice(){
     
@@ -5,23 +6,140 @@
 
         if(random === 0){
 
-            choice = "Rock";
+            choice = "rock";
 
         }else if(random === 1){
 
-            choice = "Paper";
+            choice = "paper";
 
         }else{
 
-            choice = "Scissors";
+            choice = "scissors";
 
+        }
+        console.log(choice);
+        return choice;
+    }
+
+    function getPlayerChoice(){
+
+        let playerChoice = prompt("A game of Rock, Paper, Scissors, whats your choice?");
+
+        playerChoice = playerChoice.toLowerCase();
+
+        if(playerChoice === "rock"){
+
+            choice = "rock";
+
+        }else if(playerChoice === "paper"){
+
+            choice = "paper";
+
+        }else if(playerChoice === "scissors"){
+
+            choice = "scissors";
+
+        }else{
+
+            getPlayerChoice();
         }
 
         return choice;
-}
+        
+    }
 
-    let playerChoice = prompt("A game of Rock, Paper, Scissor, whats your choice?");
+    function game(){
 
-  console.log(getComputerChoice());
+        switch(player){
 
-  console.log(playerChoice);
+        case "paper":
+
+            switch(computer){
+
+                case "paper":
+
+                    ;
+
+                break;
+
+                case "rock":
+
+                    console.log("win");
+
+                break;
+
+                case "scissors":
+
+                    console.log("lose");
+
+                break;
+
+            }
+
+        break;
+
+        case "rock":
+
+        switch(computer){
+
+            case "paper":
+
+                console.log("lose");
+
+            break;
+
+            case "rock":
+
+                console.log("draw");
+
+            break;
+
+            case "scissors":
+
+                console.log("win");
+
+            break;
+
+        }
+
+        break;
+
+        case "scissors":
+
+            switch(computer){
+
+            case "paper":
+
+                console.log("win");
+
+            break;
+
+            case "rock":
+
+                console.log("lose");
+
+            break;
+
+            case "scissors":
+
+                console.log("draw");
+
+            break;
+
+        }
+
+        break;
+
+        }
+
+    }
+
+    const computer = getComputerChoice();
+
+    const player = getPlayerChoice();
+
+    let playerPoint
+
+    game();
+
+    }
